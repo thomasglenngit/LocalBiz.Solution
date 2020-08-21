bfcc986596edcf12fe079a575d6d08b51748dde4
Thomas Glenn <thomasgrantglenn@mac.com>
2020-08-21 15:18:41 -0600

    add Resto class. 'Just realized I needed another class.

---
+++ LocalBiz/Migrations/20200821205214_Tertiary.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalBiz.Migrations
+{
+    public partial class Tertiary : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ LocalBiz/Models/Resto.cs
@@ -0,0 +1,11 @@
+namespace LocalBiz.Models
+{
+    public class Resto
+    {
+        public int RestoId { get; set; }
+        public string RName { get; set; }
+        public string RAddress { get; set; }
+        public string RType { get; set; }
+        public string RUrl { get; set; }
+    }
+}
