58c6f8e538064f6c99663c1e46156cefd1dc30c4
Thomas Glenn <thomasgrantglenn@mac.com>
2020-08-21 14:38:11 -0600

    change Display Page to Url Query and correct Query methods.

---
+++ LocalBiz/Models/UrlQuery.cs
@@ -0,0 +1,30 @@
+using Architecture.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+
+
+namespace Architecture.Models
+{
+    public class UrlQuery
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public UrlQuery()
+        {
+            this.PageNumber = 1;
+            this.PageSize = 3;
+        }
+
+        public UrlQuery(int pageNumber, int pageSize)
+        {
+            this.PageNumber = pageNumber < 1 ? 1 : pageNumber;
+            this.PageSize = pageSize > 3 ? 3 : pageSize;
+        }
+    }
+}
